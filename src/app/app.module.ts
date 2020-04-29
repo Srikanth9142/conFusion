7348b0c963538a14d3e068e23324b4df88b2d6be
srikanth9142 <srikanthtumpudi1999@gmail.com>
2020-04-29 21:22:07 +0530

    Header & Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { matchesElement } from '@angular/animations/browser/src/render/shared';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
