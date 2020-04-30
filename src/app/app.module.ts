8432b07fa4da163293250deaeeaa019179692af1
srikanth9142 <srikanthtumpudi1999@gmail.com>
2020-04-30 10:20:36 +0530

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from '../app/app-routing/app-routing.module';
+
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { matchesElement } from '@angular/animations/browser/src/render/shared';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
 import {DishService} from './services/dish.service';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
