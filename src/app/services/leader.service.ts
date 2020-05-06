c0e201077580dc6ef5280e5b79bf2d21db3909fd
srikanth9142 <srikanthtumpudi1999@gmail.com>
2020-05-06 15:02:56 +0530

    Rxjs Part1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { Observable,of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,24 +11,18 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders():Promise<Leader[]>{
+  getLeaders():Observable<Leader[]>{
     //return Promise.resolve(LEADERS);
-    return new Promise(resolve=>{
-      setTimeout(()=>resolve(LEADERS),2000);
-    });
+   return of(LEADERS).pipe(delay(2000));
   }
 
-  getLeader(id:string):Promise<Leader>{
+  getLeader(id:string):Observable<Leader>{
     //return Promise.resolve(LEADERS.filter((leader)=>leader.id === id)[0]);
-    return new Promise(resolve=>{
-      setTimeout(()=>resolve(LEADERS.filter((leader)=>leader.id===id)[0]),2000);
-    });
+    return of(LEADERS.filter((leader)=>leader.id===id)[0]).pipe(delay(2000));
   }
 
-  getFeaturedLeader():Promise<Leader>{
+  getFeaturedLeader():Observable<Leader>{
     //return Promise.resolve(LEADERS.filter((leader)=> leader.featured===true)[0]);
-    return new Promise(resolve=>{
-      setTimeout(()=>resolve(LEADERS.filter((leader)=>leader.featured===true)[0]),2000);
-    });
+   return of(LEADERS.filter((leader)=>leader.featured===true)[0]).pipe(delay(2000));
   }
 }
